5f7f4aa6021498b7df68cd301e8cee1e2ccae961
Arvinth P <arvinthp7@gmail.com>
2026-01-26 11:03:23 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,8 @@
+class Second {
+    public static void main(String[] args) {
+        int a =5;
+        int b =10;
+        System.out.println("ADDTION :" +(a+b));
+        System.out.println("Subraction :" +(a-b));
+    }
+}
