ec284ba886f08076fc0d4df83912ca7cbfcb3262
Arvinth P <arvinthp7@gmail.com>
2026-01-30 10:42:50 +0530

    Add files via upload

---
+++ Fifth.java
@@ -0,0 +1,17 @@
+class Fifth{
+    
+    public static void main(String []args){
+        int a = 700;
+        int b=90;
+        int c =78;
+        if (a >b&&c<a){
+            System.out.println("The greater numbers is a="+a);
+        }
+        else if(b>c) {
+            System.out.println("The greater number is b="+b);
+        }
+        else {
+            System.out.println("The greater numbers is c ="+c);
+        }
+    }
+}
