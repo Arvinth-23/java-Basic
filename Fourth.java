c595ec2c71614cdae86682bf08bf31af88665bd7
Arvinth P <arvinthp7@gmail.com>
2026-01-30 10:42:25 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,12 @@
+class Fourth{
+    public static void main(String []args){
+        int a = 5;
+        int b=2;
+        if (a >b){
+            System.out.println("The greater numbers is"+a);
+        }
+        else{
+            System.out.println("The number is smaller");
+        }
+    }
+}
