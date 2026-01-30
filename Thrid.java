bebfea6fbe9b458ddd889a8abb4ac283ae116681
Arvinth P <arvinthp7@gmail.com>
2026-01-30 10:03:05 +0530

    Add files via upload

---
+++ Thrid.java
@@ -0,0 +1,14 @@
+class Third{
+    public static void main(String []args){
+        int a = 5;
+        if (a >0){
+            System.out.println("The number is Positive");
+        }
+        else if (a<0){
+            System.out.println("The number is Negative");
+        }
+        else{
+            System.out.println("The number zero");
+        }
+    }
+}
