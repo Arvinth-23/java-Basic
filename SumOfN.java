78d1d0b7982941217c354a731c4392501334e611
Arvinth P <arvinthp7@gmail.com>
2026-01-31 20:39:24 +0530

    Add files via upload

---
+++ SumOfN.java
@@ -0,0 +1,9 @@
+public class SumOfN {
+    public static void main(String[] args) {
+        int N = 5; 
+        int sum = (N * (N + 1)) / 2;
+
+        System.out.println(" Output: " + sum); 
+        
+    }
+}
