18f83a05e2655f4b7a087b99833bd6b4d3c48d15
Arvinth P <arvinthp7@gmail.com>
2026-02-04 21:11:11 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+class Palindrome {
+    public static void main(String[] args) {
+        int num = 121;
+        int temp = num;
+        int rev = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            rev = (rev * 10) + digit;
+            num = num / 10;
+        }
+
+        if (temp == rev) {
+            System.out.println(temp + " is a Palindrome");
+        } else {
+            System.out.println(temp + " is not a Palindrome");
+        }
+    }
+}
