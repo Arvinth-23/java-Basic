4735140344243545d7b69cf03c43525d9f5cc6b3
Arvinth P <arvinthp7@gmail.com>
2026-02-09 21:02:21 +0530

    Add files via upload

---
+++ Exam1.java
@@ -0,0 +1,8 @@
+class Exam1{
+    public static void main(String[] args){
+        float a=11.5f;
+        float b=12.7f;
+        System.out.println(a);
+        System.out.println(b);
+        }
+}
