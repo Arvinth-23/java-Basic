b550a9404b7cc04f6053e0b3992af24151c29bc0
Arvinth P <arvinthp7@gmail.com>
2026-02-09 21:02:41 +0530

    Add files via upload

---
+++ Exam2.java
@@ -0,0 +1,11 @@
+class Exam2{
+    public static void main(String[] args) {
+        char c ='M';
+        char d ='N';
+        char s='O';
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+        
+    }
+}
