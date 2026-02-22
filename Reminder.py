1372e0fea60f472c8218c34963bdd2aefcaee1f7
Arvinth P <arvinthp7@gmail.com>
2026-02-22 12:57:21 +0530

    Add files via upload

---
+++ Reminder.py
@@ -0,0 +1,8 @@
+
+# This is simple reminder notification program that make a pop - up notication
+from plyer import notification
+notification.notify(
+    title='REMINDER',
+    message ='TAKE A BREAK AND CONTINUE TO START CODING ...',
+    timeout =10
+)
