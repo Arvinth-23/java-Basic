b364e37163dfd553148e3ac3c14a63b337204b39
Arvinth P <arvinthp7@gmail.com>
2026-03-19 20:08:10 +0530

    Add files via upload

---
+++ leet8.java
@@ -0,0 +1,20 @@
+/*This program is used to check the given location a robot can retrun back  */
+class leet8 {
+    public boolean judgeCircle(String moves) {
+        int x= 0,y=0;
+        for (char i :moves.toCharArray()){
+            
+            if(i=='U')
+                y++;
+            else if(i== 'D')
+                y--;
+            else if(i=='L')
+                x++;
+            else 
+                x--;
+        }
+        if (x == 0 && y == 0)
+            return true;
+        return false;
+    }
+}
---
+++ leet9.java
@@ -0,0 +1,20 @@
+/*This enables to check the arry has three consecutive odd number  */
+
+class leet9 {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n =arr.length;
+        int count =0;
+        for (int i =0;i<n;i++){
+            if (arr[i] %2 !=0){
+                count++;
+                if (count == 3){
+                    return true;
+                }
+                }else{
+                    count =0;
+                }
+            }
+            return false;
+        
+    }
+}
